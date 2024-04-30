7ed12028034d3ccaaec0e5831b93e5e57c5d8816
Farhad2590 <farhadhossen2590@gmail.com>
2024-04-30 13:14:56 +0600

    Vercel file Done

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ const app = express()
 const port = process.env.PORT || 5000;
 
 app.use(express.json())
-app.use(cors())
+const corsConfig = {
+    origin: '*',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    }
+    app.use(cors(corsConfig))
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cd6vky8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -26,7 +31,7 @@ async function run() {
 
 
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const productCollection = client.db('productDB').collection('product')
         const categoryCollection = client.db('productDB').collection('subcategory')
@@ -96,7 +101,7 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
